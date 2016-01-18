f8694d4554bdb52f550601ace5bfe22f7adfe2c7
Chris Siems <csiems@gmail.com>
2016-01-17 17:06:18 -0800

    completely rework file structure to accomodate database queries

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,52 +1,27 @@
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
 public class Task {
+  private int id;
+  private String description;
 
-    private String mDescription;
-    private LocalDateTime mCreatedAt;
-    private boolean mCompleted;
-    private static ArrayList<Task> instances = new ArrayList<Task>();
-    private int mId;
+  public int getId() {
+    return id;
+  }
 
-    public Task(String description) {
-        mDescription = description;
-        mCreatedAt = LocalDateTime.now();
-        mCompleted = false;
-        instances.add(this);
-        mId = instances.size();
+  public String getDescription() {
+    return description;
+  }
+
+  public Task(String description) {
+    description = description;
+  }
+
+  public static List<Task> all() {
+    String sql = "SELECT id, description FROM tasks";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
     }
 
-    public String getDescription() {
-        return mDescription;
-    }
-
-    public LocalDateTime getCreatedAt() {
-      return mCreatedAt;
-    }
-
-    public boolean isCompleted() {
-      return mCompleted;
-    }
-
-    public static ArrayList<Task> all() {
-      return instances;
-    }
-
-    public int getId() {
-      return mId;
-    }
-
-    public static Task find(int id) {
-      try {
-        return instances.get(id-1);
-      } catch (IndexOutOfBoundsException ioobe) {
-        return null;
-      }
-    }
-
-    public static void clear() {
-      instances.clear();
-    }
-
+  }
 }
