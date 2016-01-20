f674d9ededf096aca7e958aa9750562f9f078bec
Midori Bowen <midori.p.bowen@gmail.com>
2016-01-19 22:50:53 -0800

    Add One to Many With Spark. Tests fail with unknown error.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,6 +4,7 @@ import org.sql2o.*;
 public class Task {
 
   private int id;
+  private int categoryId;
   private String description;
 
   public int getId() {
@@ -14,8 +15,13 @@ public class Task {
     return description;
   }
 
-  public Task(String description) {
+  public int getCategoryId() {
+    return categoryId;
+  }
+
+  public Task(String description, int categoryId) {
     this.description = description;
+    this.categoryId = categoryId;
   }
 
   @Override
@@ -25,12 +31,13 @@ public class Task {
     } else {
       Task newTask = (Task) otherTask;
       return this.getDescription().equals(newTask.getDescription()) &&
-             this.getId() == newTask.getId();
+             this.getId() == newTask.getId() &&
+             this.getCategoryId() == newTask.getCategoryId();
     }
   }
 
   public static List<Task> all() {
-    String sql = "SELECT id, description FROM tasks";
+    String sql = "SELECT id, description, categoryId FROM tasks";
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Task.class);
     }
@@ -38,9 +45,10 @@ public class Task {
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO tasks (description) VALUES (:description)";
+      String sql = "INSERT INTO tasks (description, categoryId) VALUES (:description, :categoryId)";
       this.id = (int) con.createQuery(sql, true)
-        .addParameter("description", this.description)
+        .addParameter("description", description)
+        .addParameter("categoryId", categoryId)
         .executeUpdate()
         .getKey();
     }
@@ -53,6 +61,25 @@ public class Task {
           .addParameter("id", id)
           .executeAndFetchFirst(Task.class);
       return task;
+    }
+  }
+
+  public void update(String description) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE tasks SET description = :description) WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("description", description)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
     }
   }
 
